f019b942611db77e1c6a68c802a499f2aacfc8a3
Dinesh kumar <104176475+dineshkumarpmb@users.noreply.github.com>
2024-07-11 12:14:04 +0530

    Add files via upload

---
+++ Javascript/Samples_JS/demo.js
@@ -0,0 +1,153 @@
+/*function samsung()
+{
+    console.log("This is Samsung");
+}
+function Redmi()
+{
+    console.log("This is Redmi");
+}
+function Vivo()
+{
+    console.log("This is Vivo");
+}
+function Oppo()
+{
+    console.log("This is Oppo");
+}
+
+samsung()
+Redmi()
+Vivo()
+Oppo()
+
+
+function add()
+{
+    let a = 10;
+    let b = 20;
+    let c = a+b;
+    console.log("The addition of two numbers is: ", c)
+}
+add()
+
+
+var factor = "Suriya";
+var fcricketer = "Rohit Sharma";
+var fmovie = "7 aam Arivu";
+
+function favourite()
+{
+    console.log("Favourite Actor: ", factor);
+    console.log("Favourite Cricketer: ", fcricketer);
+    console.log("Favourite Movie: ", fmovie);
+}
+
+favourite()
+
+
+var length = 10;
+var breadth = 15;
+
+function area()
+{
+    console.log("Result: ", length*breadth);
+}
+
+area()
+
+
+function myname()
+{
+    return "Dineshkumar"
+}
+var a = myname();
+console.log(a)
+*/
+/*
+function add(a,b)
+{
+    return(a+b)
+}
+var total=add(10,20)
+console.log(total)
+*/
+//IF ELSE EXERCISES
+/*var rain = false;
+if(rain)
+    {
+        console.log("Take an Umbrella")
+    }
+else
+{
+    console.log("Enjoy the Sunshine")
+}*/
+/* var homework = false;
+if(homework)
+    {
+        console.log("Great Job!")
+    }
+else{
+    console.log("Finish your homework before playing")
+}*/
+/*var cookies = true;
+if(cookies)
+    {
+        console.log("Would you like a cookie?")
+    }
+else{
+    console.log("Time to bake more cookies..")
+}*/
+//Sample1
+/*let price= 400;
+let product = "Bags";
+let tax = 2.5866;
+console.log("Product: ",product);
+var total = price+tax;
+console.log("Total price include tax:",total.toFixed(2));
+*/
+//Sample2
+/*var FruitName = "Apple";
+var count = 6;
+var price = 45;
+var total = price * count;
+console.log("Fruits:",FruitName);
+console.log("Total Price of the fruits:",total)
+*/
+/*
+var a=15;
+if(a%2==0)
+{
+    console.log("It is Even Number..")
+}
+else{
+    console.log("It is an Odd number...")
+}*//*
+for(count=1;count<=5;count=count+1)
+{
+    console.log("Dineshkumar")
+}*/
+
+/*for(i=1;i<=10;i=i+2)
+{
+    console.log(i)
+}
+for(i/2=0;i<=10;i=i+1)
+{
+    console.log(i)
+}
+for(i=10;i>=1;i=i-1)
+    {
+        console.log(i)
+    }
+for(i=1;i<=10;i++)
+{
+    if(i%2==0)
+    {
+        console.log(i)
+    }
+}*/
+/*
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i} x 2 = ${2 * i}`);
+}
+*/
